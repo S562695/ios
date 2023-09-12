47aa3db7264e3d771e517271a9f1c68cb7099f9d
Naga Lakshmi Konanki <nagalakshmik1403@gmail.com>
2023-09-11 22:47:34 -0500

    creating username box

--- demo.playground/Contents.swift
+++ demo.playground/Contents.swift
@@ -1,4 +1,23 @@
 import UIKit
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
-var git = "Repo"
+class LoginViewController: UIViewController {
+    
+    let usernameTextField = UITextField()
+    let passwordTextField = UITextField()
+    let loginButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        usernameTextField.placeholder = "Username"
+        usernameTextField.borderStyle = .roundedRect
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(usernameTextField)
+        
+    }
+}
+
+
