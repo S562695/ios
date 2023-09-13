489414c44bf565af71f51ea04eaa08e6e31c8ec9
Naga Lakshmi Konanki <nagalakshmik1403@gmail.com>
2023-09-13 16:39:14 -0500

    Creating labels

---
+++ PlayWithAlphaNumericsApp/PlayWithAlphaNumericsApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  PlayWithAlphaNumericsApp
+//
+//  Created by Naga Lakshmi on 9/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var headerLBL: UILabel!
+    
+    
+    @IBOutlet weak var firstNumLBL: UILabel!
+    
+    
+    @IBOutlet weak var secondNumLBL: UILabel!
+    
+    @IBOutlet weak var firstStrTF: UITextField!
+    
+    @IBOutlet weak var secondStrTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+
+
+}
+
